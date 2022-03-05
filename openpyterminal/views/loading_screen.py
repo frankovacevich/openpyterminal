1ddb1d6518621243342533ba0fcd2c3fa352429d
Me <>
2022-03-05 05:29:00 -0300

    Created

---
+++ openpyterminal/views/loading_screen.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QWidget
+
+
+class LoadingScreen(QWidget):
+
+    def __init__(self, title):
+        super(LoadingScreen, self).__init__()
+        self.label.setText(title)
+
+    def update_progress(self, progress):
+        self.progressbar.maximum = 100
+        self.progressbar.value = progress
